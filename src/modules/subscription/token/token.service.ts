0122073a924b8b3c86f3f530a5c8da5eb62d6d84
SharpDevOps10 <td090833@gmail.com>
2025-06-18 12:45:51 +0300

    Add `TOKEN_DATE` for storing token date configs

--- src/modules/subscription/token/token.service.ts
+++ src/modules/subscription/token/token.service.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { randomUUID } from 'node:crypto';
 import type { ITokenService } from '@subscription/interfaces/token.service.interface';
+import { TOKEN_DATE } from '@utils/date/token.date';
 
 @Injectable()
 export class TokenService implements ITokenService {
-  private readonly ttlHours = 24;
 
   generateToken (): string {
     return randomUUID();
   }
 
   isTokenExpired (createdAt: Date): boolean {
-    const MS_PER_HOUR = 60 * 60 * 1000;
-
     const now = Date.now();
     const expirationTime = new Date(createdAt).getTime();
-    const ttlMilliseconds = this.ttlHours * MS_PER_HOUR;
+    const ttlMilliseconds = TOKEN_DATE.TTL_HOURS * TOKEN_DATE.MS_PER_HOUR;
 
     return now - expirationTime > ttlMilliseconds;
   }
