a173e534d8e6964c0bbfd79d402b7b3f4c253081
SharpDevOps10 <td090833@gmail.com>
2025-06-25 18:15:02 +0300

    Add integration test cases for `WeatherService`

--- test/mocks/handlers.ts
+++ test/mocks/handlers.ts
@@ -1,28 +1,38 @@
 import { http } from 'msw';
-import { visualCrossingParisResponse, weatherApiParisResponse } from './responses/weather.mock.responses';
+import { visualCrossingSuccessfulResponse, weatherApiParisResponse } from './responses/weather.mock.responses';
 
 export const handlers = [
   http.get('http://api.weatherapi.com/v1/current.json', ({ request }) => {
     const url = new URL(request.url);
     const city = url.searchParams.get('q');
 
-    if (city !== 'Paris') {
-      return Response.json(
-        { error: { code: 1006, message: 'No matching location found.' } },
-        { status: 404 },
-      );
+    if (city === 'Paris') {
+      return Response.json(weatherApiParisResponse, { status: 200 });
     }
 
-    return Response.json(weatherApiParisResponse, { status: 200 });
+    if (city === 'FailsEverywhere') {
+      return new Response('internal server error', { status: 500 });
+    }
+
+    return Response.json(
+      { error: { code: 1006, message: 'No matching location found.' } },
+      { status: 404 },
+    );
   }),
 
-  http.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/:city', ({ params }) => {
-    const city = params.city;
+  http.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Paris', () => {
+    return Response.json(visualCrossingSuccessfulResponse, { status: 200 });
+  }),
 
-    if (city !== 'Paris') {
-      return new Response('invalid location', { status: 400 });
-    }
+  http.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/InvalidCity', () => {
+    return new Response('invalid location', { status: 400 });
+  }),
 
-    return Response.json(visualCrossingParisResponse, { status: 200 });
+  http.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Warsaw', () => {
+    return Response.json(visualCrossingSuccessfulResponse, { status: 200 });
+  }),
+
+  http.get('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/FailsEverywhere', () => {
+    return new Response('internal server error', { status: 500 });
   }),
 ];
