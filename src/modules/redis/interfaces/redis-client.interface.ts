5cc3756aa2ed5c25ce9fa9c946c284fbd616b660
SharpDevOps10 <td090833@gmail.com>
2025-06-30 00:40:38 +0300

    Get rid of unnecessary interface

--- src/modules/redis/interfaces/redis-client.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface IRedisClient {
-  get(key: string): Promise<string | null>;
-  set(key: string, value: string, mode: 'EX', ttl: number): Promise<'OK' | null>;
-  del(key: string): Promise<number>;
-  quit(): Promise<void>;
-  on(event: 'connect', callback: () => void): this;
-  on(event: 'error', callback: (error: unknown) => void): this;
-}
