8f2dcbc9a87b9d09f732faa773722a95384dcead
SharpDevOps10 <td090833@gmail.com>
2025-07-05 15:06:53 +0300

    Move `help` section to separate file

---
+++ src/modules/metrics/constants/metric-helps.ts
@@ -0,0 +1,7 @@
+export const REDIS_METRIC_HELP = {
+  REQUESTS_TOTAL: 'Total Redis cache requests',
+  HITS_TOTAL: 'Total Redis cache hits',
+  MISSES_TOTAL: 'Total Redis cache misses',
+  ERRORS_TOTAL: 'Total Redis cache errors',
+  RESPONSE_TIME_SECONDS: 'Redis response time in seconds',
+};
