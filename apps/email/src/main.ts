5f14cd75536e8e6b5ddcea4ec7310ec8fd76eb43
SharpDevOps10 <td090833@gmail.com>
2025-07-30 15:57:35 +0300

    Get rid of unnecessary `dotenv`

--- apps/email/src/main.ts
+++ apps/email/src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { Logger } from '@nestjs/common';
-import * as dotenv from 'dotenv';
 import { EmailConfigService } from './modules/config/email-config.service';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { QUEUES } from '../../../libs/utils/constants/brokers/queues';
-dotenv.config();
+import { QUEUES } from '@utils/constants/brokers/queues';
 
 async function bootstrap (): Promise<void> {
   const app = await NestFactory.create(AppModule);
