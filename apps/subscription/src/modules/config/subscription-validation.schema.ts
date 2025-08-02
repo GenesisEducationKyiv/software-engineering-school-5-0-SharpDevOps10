c4c166ae84bdb2e9ceff79d700f821b6e7e52266
SharpDevOps10 <td090833@gmail.com>
2025-08-02 17:34:04 +0300

    Add `loki, prom-tail, grafana`

--- apps/subscription/src/modules/config/subscription-validation.schema.ts
+++ apps/subscription/src/modules/config/subscription-validation.schema.ts
@@ -3,12 +3,14 @@ import * as Joi from 'joi';
 export const subscriptionValidationSchema = Joi.object({
   PORT: Joi.number().port().default(3002).required(),
   DATABASE_URL: Joi.string().uri().required(),
+
+  POSTGRES_DB: Joi.string().required(),
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+
   TOKEN_TTL_HOURS: Joi.number().positive().required(),
 
   FRONTEND_URL: Joi.string().uri().required(),
-
-  EMAIL_CLIENT_HOST: Joi.string().hostname().required(),
-  EMAIL_CLIENT_PORT: Joi.number().port().required(),
 
   WEATHER_CLIENT_HOST: Joi.string().hostname().required(),
   WEATHER_CLIENT_PORT: Joi.number().port().required(),
