6f649368b467e3fe2834dbd9782d98b1b8faac99
SharpDevOps10 <td090833@gmail.com>
2025-08-03 17:31:51 +0300

    Implement `HttpLoggingMiddleware`

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,19 +1,29 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { GateWayConfigModule } from './modules/config/gate-way-config.module';
 import { WeatherModule } from './modules/weather/weather.module';
 import { SubscriptionModule } from './modules/subscription/subscription.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { HttpLoggingMiddleware } from './middlewares/http-logging.middleware';
+import { LoggerModule } from '@utils/modules/logger/logger.module';
 
 @Module({
   imports: [
     GateWayConfigModule,
     WeatherModule,
     SubscriptionModule,
+    LoggerModule,
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), 'apps', 'gateway', 'public'),
     }),
   ],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure (consumer: MiddlewareConsumer): void {
+    consumer
+      .apply(HttpLoggingMiddleware)
+      .forRoutes('*');
+  }
+}
+
